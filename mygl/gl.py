e7694dac658f5f6559be4911ecc0b0f9629c7ed1
andro095 <andre.rodriguezgt@yahoo.com>
2020-07-13 11:52:39 -0600

    Library almost finished

--- mygl/gl.py
+++ mygl/gl.py
@@ -21,20 +21,80 @@ def glColor(r, g, b):
     return bytes([int(255 * b), int(255 * g), int(255 * r)])
 
 
+def NDCtoWC(size, stp, num):
+    return int(((num + 1) * (size / 2)) + stp)
+
+
 WHITE = glColor(1, 1, 1)
 
 
 # Clase para todas las operaciones relacionadas con el escritor de imágenes
 class ImageCreator(object):
     # Constructor del objeto
-    def __init__(self, w, h, initColor):
+    def __init__(self, w, h, initColor, vColor):
         self.glCreateWindow(self, w, h, bg=initColor)
+        self.vColor = vColor
 
     # Creador del framebuffer
     def glCreateWindow(self, w, h, bg=glColor(0, 0, 0)):
         self.width = w
         self.height = h
-        self.bg = bg
-        self.framebuffer = [[self.bg for x in range(self.width)] for y in range(self.height)]
+        self.bgcolor = bg
+        self.glClear()
 
-    # def glViewPort(self, x=0, y=0, width=self.width-1):
+    # Creador del viewport
+    def glViewPort(self, w=0, h=0, x=0, y=0):
+        # Validamos el área del viewport
+        if x + w - 1 > self.width or y + h - 1 > self.height: return False
+        self.VPwidth = self.width - x if w == 0 else w
+        self.VPheight = self.height - y if h == 0 else h
+        self.VPXstart = x
+        self.VPYstart = y
+        return True
+
+    # Aclarar el background con su color
+    def glClear(self):
+        self.framebuffer = [[self.bgcolor for x in range(self.width)] for y in range(self.height)]
+
+    # Cambiar el bgcolor
+    def glClearColor(self, r, g, b):
+        self.bgcolor = glColor(r, g, b)
+
+    # Cambiar vColor
+    def glVertexColor(self, r, g, b):
+        self.bgcolor = glColor(r, g, b)
+
+    # Función para dibujar un punto
+    def glVertex(self, x, y):
+        if x > 1 or x < -1 or y > 1 or y < -1: return False
+        self.framebuffer[NDCtoWC(self.VPheight, self.VPYstart, y)][
+            NDCtoWC(self.VPwidth, self.VPXstart, x)] = self.vColor
+
+    # Función para hacer la image
+    def glFinish(self, filename):
+        file = open(filename, 'wb')
+
+        file.write(bytes('B'.encode('ascii')))
+        file.write(bytes('M'.encode('ascii')))
+
+        file.write(glDword(14 + 40 + self.width * self.height * 3))
+        file.write(glDword(0))
+        file.write(glDword(14 + 40))
+
+        file.write(glDword(40))
+        file.write(glDword(self.width))
+        file.write(glDword(self.height))
+        file.write(glWord(1))
+        file.write(glWord(24))
+        file.write(glDword(0))
+        file.write(glDword(self.width * self.height * 3))
+        file.write(glDword(0))
+        file.write(glDword(0))
+        file.write(glDword(0))
+        file.write(glDword(0))
+
+        for x in range(self.height):
+            for y in range(self.width):
+                file.write(self.framebuffer[x][y])
+
+        file.close()
